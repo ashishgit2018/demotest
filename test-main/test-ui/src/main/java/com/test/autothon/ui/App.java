abb3d74436894c650c349dff6eb0ab04b24d4858
ashishgit2018 <40311509+ashishgit2018@users.noreply.github.com>
2018-06-16 10:37:32 +0530

    newtestproject

---
+++ test-main/test-ui/src/main/java/com/test/autothon/ui/App.java
@@ -0,0 +1,13 @@
+package com.test.autothon.ui;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
