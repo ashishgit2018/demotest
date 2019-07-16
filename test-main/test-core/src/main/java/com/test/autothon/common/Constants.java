bf249db5c75cb1d78f0e58226057626f61f16a5e
Rahul Goyal <rahulgoyal4u@gmail.com>
2019-07-16 13:23:56 +0530

    Added code for correct Jmeter initialization

--- test-main/test-core/src/main/java/com/test/autothon/common/Constants.java
+++ test-main/test-core/src/main/java/com/test/autothon/common/Constants.java
@@ -1,7 +1,5 @@
 package com.test.autothon.common;
 
-import java.io.File;
-
 /**
  * @author Rahul_Goyal
  */
@@ -12,11 +10,6 @@ public class Constants {
     public final static String jsonResourcePath = Thread.currentThread().getContextClassLoader().getResource(env + "/json").getPath();
     public final static String jmeterResourcePath = Thread.currentThread().getContextClassLoader().getResource(env + "/jmeter").getPath();
     public final static String tempFileLocation = configResourcePath + "/temp.properties";
-    public final static String jmeterHomePath = ReadPropertiesFile.getPropertyValue("jmeter.home");
-    public final static File jmeterHome = new File(jmeterHomePath);
-    public final static String jmeterlogFilePath = "/target/" + env + "/jmeter_automation.jtl";
-    public final static String jmeterHTMLFilePath = "/target/" + env + "/jmeter/";
-    public final static String jmeterJMXFilePath = jmeterHomePath + "/" + env + "/jmeter_automation.jmx";
 
     private Constants() {
 
